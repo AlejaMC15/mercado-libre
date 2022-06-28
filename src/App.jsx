168071578659db91353c72be10254b2010437c38
amenesesc <amenesesc@indracompany.com>
2022-06-28 11:07:34 -0500

    Component routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
-import Search from './components/views/search/Search'
-import './App.scss';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Search from "./components/views/search/Search";
+import SearchResult from "./components/views/searchResult/SearchResult";
+
+import "./App.scss";
 
 function App() {
   return (
     <div className="App">
-      <Search />
+      <Routes>
+        <Route path="/" element={<Search />} />
+        <Route path="/items" element={<SearchResult />} />
+        <Route path="/items/:id" element={<SearchResult />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/views/searchResult/SearchResult.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SearchResult = () => {
+  return <div className="container">Holi</div>;
+};
+
+export default SearchResult;
